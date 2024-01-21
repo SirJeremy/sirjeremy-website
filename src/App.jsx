7f0dcab6ff429c7f26c5fe8edcb8aa05dfdfe6d8
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-01-20 19:30:35 -0800

    Added new pages to page routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import Error404Page from "./pages/Error404Page";
+import NavigationBar from "components/NavigationBar";
+
+import HomePage from 'pages/HomePage';
+import PortfolioPage from 'pages/PortfolioPage';
+import ProfilePage from "pages/ProfilePage";
+import Error404Page from "pages/Error404Page";
+
 
 function App() {
   return (
-    <div className="App antialiased w-screen h-screen">
+    <div className="antialiased relative w-screen h-screen">
+      <NavigationBar/>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
+        <Route path='/' element={<PortfolioPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
         <Route path='*' element={<Error404Page/>}/>
       </Routes>
     </div>
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function ProfilePage() {
+  return (
+    <div className='flex flex-col bg-slate-600 text-slate-100 w-full h-full'>
+        <h1 className='m-auto text-center text-3xl'>Profile</h1>
+    </div>
+  )
+}
+
+export default ProfilePage;
