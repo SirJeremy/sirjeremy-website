517c4d76aafecd8f3a4e5c00394f49a771354196
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-01-20 19:30:08 -0800

    Added basic navigation bar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { NavLink } from "react-router-dom"
+
+import githubLogo from 'assets/github-mark-white.svg';
+
+const rootStyles = "sticky top-0 bg-slate-800 text-slate-100 w-full";
+const containerStyles = "flex flex-row gap-10 py-4 mx-auto page-inset";
+const linkStyles = "text-slate-100 hover:text-slate-400";
+const activeLinkStyles = "text-blue-300";
+const iconStyles = "inline w-4 h-4 m-1 align-top ";
+
+function NavigationBar() {
+  return (
+    <div className={rootStyles}>
+        <div className={containerStyles}>
+            <NavLink to='/' className={({ isActive }) => isActive ? activeLinkStyles : linkStyles}>
+              Portfolio
+            </NavLink>
+            <NavLink to='/profile' className={({ isActive }) => isActive ? activeLinkStyles : linkStyles}>
+              About Me
+            </NavLink>
+            <a href='https://github.com/SirJeremy' target='_blank' rel='noopener noreferrer' className={linkStyles}>
+                <img src={githubLogo} alt='GitHub Logo' className={iconStyles}/>
+                GitHub
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default NavigationBar;
