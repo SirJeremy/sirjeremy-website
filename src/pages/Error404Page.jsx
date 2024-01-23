a935a61e5fc26490882388750e5457d020e1750c
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-01-22 18:32:14 -0800

    Updated 404 to new colors

--- src/pages/Error404Page.jsx
+++ src/pages/Error404Page.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 //#region Styles
 const centerDivStyles = "page-conatiner text-center m-auto";
-const headerStyles = "text-3xl pb-5";
-const textStyles = "text-xl mb-5";
-const linkStyles = "text-xl p-2 text-blue-200 border border-blue-200 rounded hover:text-blue-400 hover:border-blue-400";
+const headerStyles = "text-3xl pb-5 text-primary font-semibold";
+const textStyles = "text-xl mb-8 text-unamed-50";
+const linkStyles = "text-xl p-2 text-accent border border-accent rounded hover:text-accentl hover:border-accentl";
 //#endregion
 
 function Error404Page() {
