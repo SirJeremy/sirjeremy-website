a9da1530acb0d7c7766936172c71322daca07806
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-01-23 17:11:44 -0800

    Added system to markdown content that allows for custom elements

--- src/components/MarkdownContent.jsx
+++ src/components/MarkdownContent.jsx
@@ -5,15 +5,25 @@ import CodeBlock from 'src/components/CodeBlock';
 
 const tailwindClassMentionForCompiler = "md-img-left md-img-right md-img-center md-img-center-full";
 
-function MarkdownContent({children}) {
+const specialComponentResolution = (className, specialComponentMapping) => {
+  const firstClass = className.split(" ")[0];
+  if (specialComponentMapping !== null && firstClass in specialComponentMapping) {
+    return specialComponentMapping[firstClass]
+  }
+  return <></>;
+};
+
+// specialComponentMapping = { 'first-class-name-on-div': <MyComponent/> }
+function MarkdownContent({children, specialComponentMapping=null}) {
   return (
     <div className='prose prose-unamed max-w-none'>
       <Markdown 
         rehypePlugins={[remarkGfm, rehypeRaw]}
         children={children}
         components={{
-        code: ({node, ...props}) => <CodeBlock {...props}/>,
-        pre: ({ node, inline, className, children, ...props }) => children,
+          code: ({node, ...props}) => <CodeBlock {...props}/>,
+          pre: ({ children, ...props }) => children,
+          div: ({ node, className, ...props}) => specialComponentResolution(className, specialComponentMapping),
         }}
         />
     </div>
