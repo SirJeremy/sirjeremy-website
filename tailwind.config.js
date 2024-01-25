35c410e4c2beb92c76b7889f3011092492152bc7
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-01-24 18:39:23 -0800

    Added xs screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,9 @@ export default {
       fontFamily: {
         'sans': ['"DM Sans"', ...defaultTheme.fontFamily.sans],
       },
+      screens: {
+        'xs': '520px',
+      },
     },
   },
   plugins: [
