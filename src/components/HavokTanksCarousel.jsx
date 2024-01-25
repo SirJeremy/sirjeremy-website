d8ec88b487a925ea885268efd102bac6bbcd2d12
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-01-24 18:02:04 -0800

    Added missing image and added alt text for Havok Tanks

--- src/components/HavokTanksCarousel.jsx
+++ src/components/HavokTanksCarousel.jsx
@@ -4,6 +4,7 @@ import { Carousel } from 'flowbite-react';
 import useElementSize from 'src/hooks/useElementSize';
 
 import mainMenu from "src/assets/ht/main-menu.png";
+import levelSelect from "src/assets/ht/level-select.png";
 import boostRamp from "src/assets/ht/boost-ramp.png";
 import crew from "src/assets/ht/crew.png";
 import combat1 from "src/assets/ht/combat-1.png";
@@ -26,17 +27,19 @@ function HavokTanksCarousel({className=''}) {
     setHeight(imgWidth * widthToHeightRatio);
   }, [imgWidth]);
 
+  // TODO: Add alt text
   return (
     <div className={className + ' not-prose'} style={{height: `${height}px`}}>
       <Carousel slideInterval={7500} pauseOnHover>
-        <img src={mainMenu} alt="..." ref={imgRef}/>
-        <img src={boostRamp} alt="..."/>
-        <img src={crew} alt="..."/>
-        <img src={combat1} alt="..."/>
-        <img src={lavaLevel} alt="..."/>
-        <img src={combat2} alt="..."/>
-        <img src={sandLevel} alt="..."/>
-        <img src={endGame} alt="..."/>
+        <img src={mainMenu} alt='Screenshot of main menu screen of the game "Havoc Tanks". The background is a hangar containing blue tanks.' ref={imgRef}/>
+        <img src={levelSelect} alt="Screenshot of characater select screen located in a hangar, where orange team is selecting their tank type."/>
+        <img src={boostRamp} alt="Screenshot of game with Havoc Tanks launching off boost ramps."/>
+        <img src={crew} alt="Screenshot of game of Crew drones exchanging fire."/>
+        <img src={combat1} alt="Screenshot of game with two Havoc Tanks engaged in combat. The oragne tank had just been hit and recoiling from the impact."/>
+        <img src={lavaLevel} alt="Screenshot of game with the blue side launching an assault on a gate, and the orange side manuvaring over lava."/>
+        <img src={combat2} alt="Screenshot of game with two Havoc Tanks engaged in combat located in the orange base near the core."/>
+        <img src={sandLevel} alt="Screenshot of game with two Havoc Tanks submiting a sandy hill."/>
+        <img src={endGame} alt="Screenshot of game during game over screen. It shows that the blue side won and the orange side lost."/>
       </Carousel>
     </div>
   )
