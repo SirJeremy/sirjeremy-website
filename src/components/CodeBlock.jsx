fcf235e31b549d7a77fcd156dd475a4c2bd2904f
Jeremy Cortez <sirjeremycortez@gmail.com>
2024-02-04 05:56:23 -0800

    Fixed width on code block

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,11 +1,11 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark as highlighTheme } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-const blockStyles = "not-prose max-w-[660px]";
+const blockStyles = "not-prose w-full";
 const inlineStyles = "not-prose bg-[#1d1f21] text-[#c5c8c6] rounded-sm pl-1 pr-1 ml-0.5 mr-0.5";
 
 const CodeBlock = ({inline, className, children}) => {
-    const match = /language-(\w+)/.exec(className || '')
+    const match = /language-(\w+)/.exec(className || '');
     return !inline && match ? (
         <SyntaxHighlighter
             className={blockStyles}
